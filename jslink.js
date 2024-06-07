d46aa97c2449078f9c3fad6270b54dfd16f0085a
Brian Ali <gamerfunky78@gmail.com>
2024-06-07 18:17:41 +0300

    Add files via upload

---
+++ jslink.js
@@ -0,0 +1,19 @@
+    
+    document.getElementById("blank")
+
+    function react() {  
+
+        alert("coming soon");
+    
+    }
+    document.getElementsByClassName("illustration")
+    function react2() {
+
+        alert("home");
+    
+    }
+
+    document.getElementsByClassName("project1")
+
+
+
